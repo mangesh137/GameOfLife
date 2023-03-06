cb15f2eaf75047d710ca17058c8d05e6d5cc59b3
Mangesh Udawant <mangesh.udawant@swiggy.in>
2023-03-06 17:07:44 +0530

    changes

---
+++ src/com/swiggy/GameOfLife.java
@@ -0,0 +1,92 @@
+package com.swiggy;
+
+public class GameOfLife {
+
+    public static void drawGrid(int[][] board, int row, int column) {
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < column; j++) {
+                System.out.print(" " + board[i][j]);
+            }
+            System.out.println();
+        }
+    }
+
+    public static int[][] nextGeneration(int board[][], int rows, int columns) {
+        int nextGenerationBoard[][] = new int[rows][columns];
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < columns; j++) {
+                int aliveNeighboursCount = getAliveNeighboursCount(board, rows, columns, i, j);
+                if ((board[i][j] == 1) && (aliveNeighboursCount < 2 || aliveNeighboursCount > 3)) {
+                    nextGenerationBoard[i][j] = 0;
+                } else if ((board[i][j] == 0) && (aliveNeighboursCount == 3)) {
+                    nextGenerationBoard[i][j] = 1;
+                } else {
+                    nextGenerationBoard[i][j] = board[i][j];
+                }
+            }
+        }
+        return nextGenerationBoard;
+    }
+
+    public static int getAliveNeighboursCount(int board[][], int rows, int columns, int r, int c) {
+        int aliveNeighbours = 0;
+        for (int i = -1; i <= 1; i++) {
+            for (int j = -1; j <= 1; j++) {
+                if ((r + i >= 0 && r + i < rows) && (c + j >= 0 && c + j < columns)) {
+                    aliveNeighbours += board[r + i][c + j];
+                }
+            }
+        }
+        aliveNeighbours -= board[r][c];
+        return aliveNeighbours;
+    }
+
+    public static void main(String[] args) {
+        //int row = 10 , column = 10;
+        /*int[][] board = {{0, 0, 1, 1, 1, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 1, 1},
+                {0, 0, 0, 0, 0, 0, 0, 0, 1, 0}};*/
+
+        /*int[][] board = {{0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 1, 1, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 1, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0}};*/
+
+        /*int[][] board = {{0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 1, 1, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 1, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 1, 1, 0, 0, 0, 0, 0},
+                {0, 0, 1, 1, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 1, 0, 0, 0, 0},
+                {0, 0, 0, 0, 1, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0}};*/
+        int[][] board = {
+                {1, 0, 1, 1},
+                {1, 1, 0, 0},
+                {1, 1, 0, 1},
+                {0, 1, 1, 0},
+                {0, 0, 0, 0}
+        };
+        int row = 5 , column = 4;
+        System.out.println("-------------Initial------------");
+        drawGrid(board, row, column);
+        System.out.println("-------------Next Generation------------");
+        drawGrid(nextGeneration(board, row, column), row, column);
+
+    }
+}
